3867525291859ab9bf6b83355e093ca3cea88197
Nilton Vasques <nilton.vasques@gmail.com>
2023-02-23 15:40:07 -0300

    refactor

--- background.js
+++ background.js
@@ -1,7 +1,6 @@
 chrome.commands.onCommand.addListener(function(command) {
   console.log('Command:', command)
   if (command === "toggle-github-buttons") {
-    console.log("GITHUB BUTTONS TOGGLE")
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
       chrome.tabs.sendMessage(tabs[0].id, {action: "toggleButtons"});
     });
