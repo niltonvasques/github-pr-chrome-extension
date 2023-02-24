d398985225135999453c6ee9a36f18ad618f7363
leocostaba <leocostaba1@gmail.com>
2023-02-24 17:13:16 -0300

    Add re-request-review command

--- background.js
+++ background.js
@@ -1,8 +1,15 @@
+function sendCommandToTab(command) {
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, {action: command});
+  });
+}
+
 chrome.commands.onCommand.addListener(function(command) {
-  console.log('Command:', command)
+  console.log('Command:', command);
   if (command === "close-github-pr-files") {
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, {action: "closePRFiles"});
-    });
+    sendCommandToTab("closePRFiles");
+  }
+  if (command === "re-request-review") {
+    sendCommandToTab("requestReview");
   }
 });
