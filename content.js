998e3db4b270bbf535de575843b90b40baa60a8d
Nilton Vasques <nilton.vasques@gmail.com>
2025-09-30 11:36:03 -0300

    Fix closePRFiles after new github update

--- content.js
+++ content.js
@@ -1,8 +1,7 @@
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   if (request.action === "closePRFiles") {
-    Array.prototype.slice.call(document.getElementsByClassName("btn-octicon"))
-      .filter((btn) => btn.attributes['aria-expanded']?.value === "true")
-      .forEach((btn) => { btn.click() });
+    Array.prototype.slice.call(document.getElementsByClassName("octicon-chevron-down"))
+      .forEach((svg) => svg.parentNode.click());
   }
 
   if (request.action === "requestReview") {
